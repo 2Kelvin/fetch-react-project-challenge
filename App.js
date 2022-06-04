5007641ad434fcb1b2a9d70b73040f0b25b7ada8
2Kelvin <kvin.mwangi254@gmail.com>
2022-06-04 06:59:38 +0300

    fetch API app

---
+++ App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="app">
+      <div className="divButton">Users</div>
+      <div className="divButton">Posts</div>
+      <div className="divButton">Comments</div>
+    </div>
+  );
+}
+
+export default App;
