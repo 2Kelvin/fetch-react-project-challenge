8e26be1dd0ab13913f7c65f1b352ac751366adf1
2Kelvin <kvin.mwangi254@gmail.com>
2022-06-08 21:16:54 +0300

    fetched post data, should set state with it

--- App.js
+++ App.js
@@ -1,9 +1,39 @@
+import { useState } from "react";
+
 function App() {
+  const [users, setUsers] = useState("");
+
+  const getUsers = async () => {
+    const response = await fetch("http://jsonplaceholder.typicode.com/users", {
+      method: "GET",
+      headers: {
+        Accept: "application/json"
+      }
+    });
+
+    const usersData = await response.json();
+    setUsers(
+      <ul>
+      {usersData.map(user => <li key={user.id}>{user}</li>)}
+      </ul>
+    );
+  };
+
   return (
     <div className="app">
-      <button>Users</button>
-      <button>Posts</button>
-      <button>Comments</button>
+      <div className="topButtons">
+        <button
+          onClick={()=>getUsers()}
+        >Users</button>
+
+        <button>Posts</button>
+
+        <button>Comments</button>
+      </div>
+
+      <div className="content">
+       {users}
+      </div>
     </div>
   );
 }
