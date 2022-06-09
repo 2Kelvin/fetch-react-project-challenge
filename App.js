3e763291ce3536867f404d497ef09fea8b5ebb78
2Kelvin <kvin.mwangi254@gmail.com>
2022-06-09 07:40:15 +0300

    successfully fetched all data; on button click API data is displayed on page

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 function App() {
-  const [users, setUsers] = useState("");
+  const [apiData, setAPIdata] = useState("");
 
   const getUsers = async () => {
     const response = await fetch("http://jsonplaceholder.typicode.com/users", {
@@ -15,9 +15,33 @@ function App() {
     return usersData;
   };
 
-  const displayUsersData = async () => {
-    const data = await getUsers();
-    setUsers(
+  const getPosts = async () => {
+    const response = await fetch("http://jsonplaceholder.typicode.com/posts", {
+      method: "GET",
+      headers: {
+        Accept: "application/json"
+      }
+    });
+
+    const postsData = await response.json();
+    return postsData;
+  };
+
+  const getComments = async () => {
+    const response = await fetch("http://jsonplaceholder.typicode.com/comments", {
+      method: "GET",
+      headers: {
+        Accept: "application/json"
+      }
+    });
+
+    const commentsData = await response.json();
+    return commentsData;
+  };
+
+  const displayAPIdata = async (asyFunc) => {
+    const data = await asyFunc();
+    setAPIdata(
       data.map(user =>
         <li key={user.id} style={{marginBottom:'20px'}}>
           {JSON.stringify(user)}
@@ -28,14 +52,12 @@ function App() {
   return (
     <div className="app">
       <div className="topButtons">
-        <button onClick={() => displayUsersData()}> Users </button>
-        <button>Posts</button>
-        <button>Comments</button>
+        <button onClick={() => displayAPIdata(getUsers)}> Users </button>
+        <button onClick={() => displayAPIdata(getPosts)}> Posts </button>
+        <button onClick={() => displayAPIdata(getComments)}> Comments </button>
       </div>
 
-      <ul className="content">
-       {users}
-      </ul>
+      <ul className="content"> {apiData} </ul>
     </div>
   );
 }
